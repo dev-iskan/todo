db8cde01fafd4c9006a645baac0aef4cce389162
dev-iskan <successfuliskan@gmail.com>
2018-07-23 17:21:59 +0500

    added guide to project

--- database/migrations/2018_07_23_051017_create_tasks_table.php
+++ database/migrations/2018_07_23_051017_create_tasks_table.php
@@ -17,7 +17,6 @@ class CreateTasksTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('priority')->nullable();
-
             $table->timestamps();
         });
     }
